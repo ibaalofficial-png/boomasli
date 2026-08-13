acfe4afde293fe9703599e7311613046ce743d72
ibaalofficial-png <ibaalofficial@gmail.com>
2026-08-13 09:31:25 +0700

    Refactor AppDelegate methods for better readability

--- BoomMusic/AppDelegate.swift
+++ BoomMusic/AppDelegate.swift
@@ -5,18 +5,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+        didFinishLaunchingWithOptions launchOptions:
+            [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
 
-        AudioManager.shared.setupAudioSession()
+        AudioManager.shared.configure()
 
         return true
     }
 
     func application(
         _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
+        configurationForConnecting connectingSceneSession:
+            UISceneSession,
+        options:
+            UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
 
         return UISceneConfiguration(
